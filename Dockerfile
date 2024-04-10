8b0a95ac2f56b74d264afb0463d5613457e8744f
Swarnim Arun <swarnimarun11@gmail.com>
2024-04-11 03:05:36 +0530

    demo docker compose

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,24 @@
 FROM rust:1.77-slim-bookworm as builder
-ADD . /code
+RUN mkdir -p /code/src && echo "fn main() {}" > /code/src/main.rs
+ADD Cargo.toml /code
+ADD Cargo.lock /code
 WORKDIR /code
 RUN update-ca-certificates \
     && apt-get update \
     && apt-get install -y --no-install-recommends build-essential cmake
-RUN cargo build --release
+RUN cargo build
+RUN rm -rf /code/src && mkdir /code/src
+COPY src/main.rs /code/src/
+RUN cargo build
 RUN rm -rf /var/lib/apt/lists/*
 
 FROM debian:stable-slim
-COPY --from=builder /code/target/release/ai-gateway /
+
+WORKDIR /
+COPY --from=builder /code/target/debug/ai-gateway /
 # copy the aispec.toml for the gateway
-COPY --from=builder /code/.aispec.toml /
+COPY aispec.docker.toml /aispec.toml
+
 EXPOSE 8000
 EXPOSE 8080
 CMD [ "./ai-gateway" ]
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-use std::{collections::HashMap, path::PathBuf};
+use std::path::PathBuf;
 
 use async_trait::async_trait;
+// use prometheus::register_int_counter;
 use structopt::StructOpt;
+use tracing::info;
 
 use pingora_core::server::configuration::Opt;
 use pingora_core::server::Server;
@@ -25,7 +27,7 @@ pub struct Model {
 #[derive(Debug, Deserialize, Serialize)]
 pub struct GatewaySpecification {
     enabled: Vec<String>,
-    models: HashMap<String, Model>,
+    models: std::collections::HashMap<String, Model>,
 }
 
 fn files_at(path: PathBuf, files: &[&str]) -> Vec<PathBuf> {
@@ -46,14 +48,27 @@ impl GatewaySpecification {
     }
 }
 
-pub struct AiGateway {
+pub struct MyGateway {
     specification: GatewaySpecification,
 }
 
 #[async_trait]
-impl ProxyHttp for AiGateway {
+impl ProxyHttp for MyGateway {
     type CTX = ();
     fn new_ctx(&self) -> Self::CTX {}
+
+    async fn upstream_request_filter(
+        &self,
+        _session: &mut Session,
+        upstream_request: &mut pingora_http::RequestHeader,
+        _ctx: &mut Self::CTX,
+    ) -> Result<()> {
+        // _session.req_header().headers.get("Host")
+        upstream_request
+            .insert_header("Host", "one.one.one.one")
+            .unwrap();
+        Ok(())
+    }
 
     async fn request_filter(&self, session: &mut Session, _ctx: &mut Self::CTX) -> Result<bool> {
         if self.specification.enabled.len() == 0 {
@@ -68,24 +83,20 @@ impl ProxyHttp for AiGateway {
         session: &mut Session,
         _ctx: &mut Self::CTX,
     ) -> Result<Box<HttpPeer>> {
-        let mut request: Vec<u8> = vec![];
-        request.reserve(10240);
-        while let Ok(Some(bytes)) = session.read_request_body().await {
-            request.extend(&bytes[..]);
-        }
-        let json_body: serde_json::Value = serde_json::de::from_slice(&request).unwrap();
-        if let Some(model_name) = json_body.get("model_name") {
-            if let Some(mn) = model_name.as_str() {
-                if let Some(model) = self.specification.models.get(mn) {
-                    let addr = (model.ipaddr.as_str(), model.port);
-                    // empty sni is fine as tls is set to "false"!
-                    let sni = String::new();
-                    let peer = Box::new(HttpPeer::new(addr, false, sni));
+        if let Some(model) = session.req_header().headers.get("Model") {
+            if let Ok(m) = model.to_str() {
+                if let Some(m) = self.specification.models.get(m) {
+                    let addr = (m.ipaddr.as_str(), m.port);
+                    info!("connecting to {addr:?}");
+
+                    let peer = Box::new(HttpPeer::new(addr, false, "one.one.one.one".to_string()));
                     return Ok(peer);
                 }
             }
         }
-        panic!("unimplemented!");
+        return Err(pingora_core::Error::new(pingora_core::ErrorType::Custom(
+            "Failed to get address for upstream",
+        )));
     }
 
     async fn response_filter(
@@ -98,11 +109,10 @@ impl ProxyHttp for AiGateway {
         Self::CTX: Send + Sync,
     {
         // replace existing header if any
-        upstream_response
-            .insert_header("Server", "MyGateway")
-            .unwrap();
-
-        // we don't support http3 yet, disable manually!
+        // upstream_response
+        //     .insert_header("Server", "MyGateway")
+        //     .unwrap();
+        // because we don't support h3
         upstream_response.remove_header("alt-svc");
 
         Ok(())
@@ -117,15 +127,17 @@ impl ProxyHttp for AiGateway {
         let response_code = session
             .response_written()
             .map_or(0, |resp| resp.status.as_u16());
-        tracing::info!(
+        info!(
             "{} response code: {response_code}",
             self.request_summary(session, ctx)
         );
+
+        // self.req_metric.inc();
     }
 }
 
-// RUST_LOG=INFO cargo run
-// curl 127.0.0.1:6191 -H "Content-Type: application/json" --data '{"messages": [{"role": "user", "content": "Who is Newton?"}]}'
+// RUST_LOG=INFO cargo run --example load_balancer
+// curl 127.0.0.1:6191 -H "Host: one.one.one.one"
 // curl 127.0.0.1:6190/family/ -H "Host: one.one.one.one"
 // curl 127.0.0.1:6191/login/ -H "Host: one.one.one.one" -I -H "Authorization: password"
 // curl 127.0.0.1:6191/login/ -H "Host: one.one.one.one" -I -H "Authorization: bad"
@@ -141,15 +153,16 @@ fn main() {
     let mut my_server = Server::new(Some(opt)).unwrap();
     my_server.bootstrap();
 
-    let mut gateway = pingora_proxy::http_proxy_service(
+    let mut my_proxy = pingora_proxy::http_proxy_service(
         &my_server.configuration,
-        AiGateway {
+        MyGateway {
             specification: GatewaySpecification::load_from_cwd()
                 .expect("failed to get AI Gateway Specification at cwd('.aispec.toml')"),
+            // req_metric: register_int_counter!("reg_counter", "Number of requests").unwrap(),
         },
     );
-    gateway.add_tcp("0.0.0.0:6191");
-    my_server.add_service(gateway);
+    my_proxy.add_tcp("0.0.0.0:6191");
+    my_server.add_service(my_proxy);
 
     // let mut prometheus_service_http =
     //     pingora_core::services::listening::Service::prometheus_http_service();
